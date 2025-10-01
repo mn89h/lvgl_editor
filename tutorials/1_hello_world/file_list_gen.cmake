e1565ab902774846f16d7215b1624549dbb7c993
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-10-01 13:36:07 +0200

    rebuilild the previews

---
+++ tutorials/1_hello_world/file_list_gen.cmake
@@ -0,0 +1,7 @@
+list(
+  APPEND
+  PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_hello_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_lib_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_lib.c
+  ${CMAKE_CURRENT_LIST_DIR}/fonts/font_medium_data.c)
---
+++ tutorials/6_data_binding/file_list_gen.cmake
@@ -0,0 +1,8 @@
+list(
+  APPEND
+  PROJECT_SOURCES
+  ${CMAKE_CURRENT_LIST_DIR}/components/round_button_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/components/sliderbox_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/screens/screen_data_bindings_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_lib_gen.c
+  ${CMAKE_CURRENT_LIST_DIR}/ui_lib.c)
